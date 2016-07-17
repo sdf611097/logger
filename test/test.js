5994b85b801895dbc2ec38d5aa8c20fcbc216437
Jim Lin <sdf611097@gmail.com>
2016-07-17 14:47:43 +0800

    show possible usages, and add screenshot

--- test/test.js
+++ test/test.js
@@ -2,39 +2,28 @@
 'use strict';
 const logger = require('../index.js');
 
-console.log('colorsList', logger.COLORS);
-console.log('effectsList', logger.EFFECTS);
+console.log('colors:', logger.COLORS.join(','));
+console.log('effects:', logger.EFFECTS.join(','));
 
+//logger.colorLog(colorName, ...)
 for(let index in logger.COLORS){
     const color = logger.COLORS[index];
     logger.colorLog(color, 'this', 'is', 'a/an', color, 'log');
 }
 
-function shuffle(arr){
-    const times = arr.length * 100;
-    for(let i = 0; i< times; i++){
-        let a = randIndex(arr);
-        let b = randIndex(arr);
-
-        let tmp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = tmp;
+//logger.log(opts, ...)
+const noResetEffects = logger.EFFECTS.filter(effect=> effect!= "RESET");
+const color = logger.COLORS[3];
+for(let i=0; i< Math.pow(2,noResetEffects.length); i++){
+    let effects = [];
+    for(let j=0, tmp=i; j<noResetEffects.length; j++){
+        if(tmp%2 ===1){
+            effects.push(noResetEffects[j]);
+        }
+        tmp = tmp >> 1;
     }
-    return arr;
-}
-
-function randIndex(arr){
-    return Math.floor(Math.random() * arr.length);
-}
-
-
-const NOT_EXIST = 'notExist';
-let effectsWithNotExist = logger.EFFECTS;
-effectsWithNotExist.push(NOT_EXIST);
-
-for(let i=0; i< 20; i++){
-    let effects = shuffle(effectsWithNotExist).slice(0,2);
-    let color = [logger.COLORS[randIndex(logger.COLORS)]];
-    const test = color.concat(effects);
-    logger.log(test, 'this', 'is', 'a/an', test.filter(opt=>opt!==NOT_EXIST).join('-'), 'log');
+    let test = [color].concat(effects);
+    if(effects.length>0){
+        logger.log(test, 'this', 'is', 'a/an', test.join('-'), 'log');
+    }
 }
