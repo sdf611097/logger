63adef9b3ee972517fd6f90b4aedb0cb7adc0876
Jim Lin <sdf611097@gmail.com>
2016-07-17 15:46:50 +0800

    apply jscs, jshint

--- index.js
+++ index.js
@@ -20,69 +20,71 @@ const effects = {
 
 const supportedCodes = Object.assign({}, colors, effects);
 
-
 const END = '\x1b[0m';
 
 const text = 'test';
 
 //getCode, is not exist, using white
-function getCode(colorName){
+function getCode(colorName) {
     colorName = colorName.toUpperCase();
-    return colors[colorName]? colors[colorName]: getCode('white');
+    return colors[colorName] ? colors[colorName] : getCode('white');
 }
 
 //log(color, arg1, arg2, ..., argN)
-function colorLog(color){
+function colorLog(color) {
     const code = getCode(color);
     let args =  Array.prototype.slice.call(arguments, 0);
     args[0] = code;
     _log.apply(null, args);
 }
 
-function getBegin(codes){
-    if( Object.prototype.toString.call( codes ) !== '[object Array]' ) {
+function getBegin(codes) {
+    if (Object.prototype.toString.call(codes) !== '[object Array]') {
         return getBegin(['white']);
     }
+
     return '\x1b[' + codes.join(';') + 'm';
 }
 
-function _log(codes){
-    if(typeof codes === "number"){
+function _log(codes) {
+    if (typeof codes === 'number') {
         codes = [codes];
     }
+
     let args = [getBegin(codes)];
     args = args.concat(Array.prototype.slice.call(arguments, 1));
     args = args.concat([END]);
     console.log.apply(null, args);
 }
 
-function log(opts){
-    if(typeof opts === "string"){
+function log(opts) {
+    if (typeof opts === 'string') {
         opts = [opts];
     }
 
-    if( Object.prototype.toString.call( opts ) !== '[object Array]' ) {
+    if (Object.prototype.toString.call(opts) !== '[object Array]') {
         opts = [];
     }
 
     const getCode = name=> supportedCodes[name.toUpperCase()];
+
     //remove undefined, if(opt) will ignore RESET, using || to apply 0
     //and then retreive its code
-    const codes = opts.filter(opt=> getCode(opt) || getCode(opt)===0).map(getCode);
+    const codes = opts.filter(opt=> getCode(opt) || getCode(opt) === 0).map(getCode);
     let args =  Array.prototype.slice.call(arguments);
     args[0] = codes;
     _log.apply(null, args);
 }
 
-function stack(opts, maxLines){
-    let stack = new Error("This is a stack log, not an Error").stack;
+function stack(opts, maxLines) {
+    let stack = new Error('This is a stack log, not an Error').stack;
     let lines = stack.split('\n').slice(2);
     lines.slice(0, maxLines).forEach(line=> log(opts, line));
 }
 
-function debug(){
-    const opts = ['cyan','italic'];
-    if(process.env.VERBOSE){
+function debug() {
+    const opts = ['cyan', 'italic'];
+    if (process.env.VERBOSE) {
         log.apply(null, [opts].concat(Array.prototype.slice.call(arguments)));
     }
 }
--- test/test.js
+++ test/test.js
@@ -6,43 +6,46 @@ console.log('colors:', logger.COLORS.join(','));
 console.log('effects:', logger.EFFECTS.join(','));
 
 //logger.colorLog(colorName, ...)
-for(let index in logger.COLORS){
+for (let index in logger.COLORS) {
     const color = logger.COLORS[index];
     logger.colorLog(color, 'this', 'is', 'a/an', color, 'log');
 }
 
 //logger.log(opts, ...)
-const noResetEffects = logger.EFFECTS.filter(effect=> effect!= "RESET");
+const noResetEffects = logger.EFFECTS.filter(effect=> effect != 'RESET');
 const color = logger.COLORS[3];
-for(let i=0; i< Math.pow(2,noResetEffects.length); i++){
+for (let i = 0; i < Math.pow(2, noResetEffects.length); i++) {
     let effects = [];
-    for(let j=0, tmp=i; j<noResetEffects.length; j++){
-        if(tmp%2 ===1){
+    for (let j = 0, tmp = i; j < noResetEffects.length; j++) {
+        if (tmp % 2 === 1) {
             effects.push(noResetEffects[j]);
         }
+
         tmp = tmp >> 1;
     }
+
     let test = [color].concat(effects);
-    if(effects.length>0){
+    if (effects.length > 0) {
         logger.log(test, 'this', 'is', 'a/an', test.join('-'), 'log');
     }
 }
 
-function showStack(){
+function showStack() {
     let args = arguments;
-    function func(){
+    function func() {
         logger.stack();
         console.log('###');
-        logger.stack(['red','bold'], 3);
+        logger.stack(['red', 'bold'], 3);
     }
+
     func();
 }
+
 showStack();
 
-
-function testDebug(){
+function testDebug() {
     console.log('process.env.VERBOSE', process.env.VERBOSE);
-    logger.debug('hi','debug','log');
+    logger.debug('hi', 'debug', 'log');
     console.log('end of log');
 }
 
